53409c18667b4ed5e64737ca191f890778b4be08
bbpink <ssbbpink@gmail.com>
2017-04-12 07:56:07 +0000

    base dev

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var ECT = require("ect");
+var sass = require("node-sass");
 var session = require("express-session");
 var bodyParser = require("body-parser");
 var sqlite3 = require("sqlite3");
@@ -11,5 +12,17 @@ app.set('view engine', 'ect');
 app.engine("ect", ectRenderer.render);
 app.use(bodyParser.urlencoded({ extended: false }));
 var db = new sqlite3.Database("./data/db");
+
+// assets
+app.get("/index.css", function(req, res) {
+  var f = sass.renderSync({file: __dirname + "/assets/index.scss"});
+  res.writeHead(200, {"Content-Type":"text/css"});
+  res.end(f.css, "utf-8");
+});
+
+// routes
+app.get("/", function(req, res) {
+  res.render("index");
+});
 
 app.listen(3000);
